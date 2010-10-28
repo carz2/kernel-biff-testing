938c70e99f54fdfd86f75ec9f7c466942e093d41
ezterry <ezterry3@gmail.com>
2010-10-28 08:55:46 -0400

    Revert "I believe this is required for DS; as is done in ASOP"
    The usb unplug power draw is much much worse with it
    so if we need it we have another bug to zap first
    
    This reverts commit 0af5f70930208c35309dd1ac10c70451f1e4eee6.

--- drivers/usb/gadget/msm72k_udc.c
+++ drivers/usb/gadget/msm72k_udc.c
@@ -1242,10 +1242,6 @@ static void usb_do_work(struct work_struct *w)
 #ifdef CONFIG_ARCH_MSM_SCORPION
 				usb_phy_reset(ui);
 #endif
-				/* clear for DS as well */
-#ifdef CONFIG_ARCH_MSM_ARM11
-				usb_phy_reset(ui);
-#endif
 
 				/* power down phy, clock down usb */
 				spin_lock_irqsave(&ui->lock, iflags);
